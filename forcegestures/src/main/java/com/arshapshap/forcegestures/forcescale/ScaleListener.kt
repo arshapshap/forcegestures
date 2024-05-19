59f458109b168e4120443fa61d39632341af61fb
arshapshap <arshapshap@mail.ru>
2024-05-19 19:01:37 +0300

    Fix force scaling

--- forcegestures/src/main/java/com/arshapshap/forcegestures/forcescale/ScaleListener.kt
+++ forcegestures/src/main/java/com/arshapshap/forcegestures/forcescale/ScaleListener.kt
@@ -4,6 +4,7 @@ import android.view.MotionEvent
 import android.view.ScaleGestureDetector
 import com.arshapshap.forcegestures.PressureHelper
 
+private const val MITIGATION_FACTOR = 0.1f
 class ScaleListener(
     private val onPinch: (Float) -> Unit
 ) : ScaleGestureDetector.SimpleOnScaleGestureListener() {
@@ -13,8 +14,9 @@ class ScaleListener(
     override fun onScale(detector: ScaleGestureDetector): Boolean {
         var scaleFactor = detector.scaleFactor
         motionEvent?.let {
-            val deviance = (PressureHelper.getPressureDeviance(it) - 1) * 0.1f + 1
-            scaleFactor *= if (scaleFactor >= 1) deviance else 1/deviance
+            val deviance = (PressureHelper.getPressureDeviance(it) - 1) * MITIGATION_FACTOR + 1
+            scaleFactor = if (scaleFactor >= 1) maxOf(scaleFactor, scaleFactor * deviance)
+                else minOf(scaleFactor, scaleFactor / deviance)
         }
         onPinch.invoke(scaleFactor)
         return true
